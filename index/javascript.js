ace99e9923988dca54d030d41b79bcd4f8d245e1
Maks <38703945+MaksOnashko@users.noreply.github.com>
2018-07-01 20:35:37 +0300

    3

--- index/javascript.js
+++ index/javascript.js
@@ -1,6 +1,32 @@
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
 }
 
-const element = <Welcome name="Sara" />;
-ReactDOM.render(element, document.getElementById('root'));
+const domContainer = document.querySelector('#root');
+ReactDOM.render(e(LikeButton), domContainer);
+
+//function Welcome(props) {
+//  return <h1>Hello, {props.name}</h1>;
+//}
+//
+//const element = <Welcome name="Sara" />;
+//ReactDOM.render(element, document.getElementById('root'));
